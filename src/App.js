0f016879de6d9cfde626b94184d2964fefaf90b9
george najm <georgenajm28@yahoo.com>
2018-06-16 19:01:01 -0400

    nav and footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Nav from "../src/components/nav";
+import Footer from "../src/components/footer";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hi
+        <Nav />
+
+        <Footer />
       </div>
     );
   }

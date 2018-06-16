f46f7864d71305438d68fdf1b2d6cff13261dd9c
george najm <georgenajm28@yahoo.com>
2018-06-16 19:32:54 -0400

    only rendering event on exact route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Nav from "../src/components/nav";
 import Footer from "../src/components/footer";
@@ -10,12 +10,14 @@ import EventPage from "../src/components/eventPage";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Nav />
-        <Home />
-        <EventPage />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Home />
+          <Route exact path={`/event`} component={EventPage} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }

455c0478744463cc1f64fd704ab4f127c65cf47d
george najm <georgenajm28@yahoo.com>
2018-06-18 00:46:04 -0400

    added tests on nav

--- cypress/integration/home page/home_spec.js
+++ cypress/integration/home page/home_spec.js
@@ -2,18 +2,32 @@ describe("On home page" ,function(){
   beforeEach(function() {
     cy.visit('http://localhost:3000/');
   });
+  context('Navbar', function(){
 
-  context('On large screen', function(){
-
-    context('Navbar', function(){
-      it('contains search field', function() {
+    it('Is present', function() {
+      cy.get('.nav');
+    });
+    context('Search', function() {
+      it('Is present', function() {
         cy.get('.input-field');
       });
-      it('Search is focused', function() {
+      it('Is focused', function() {
         cy.focused().should('have.class','input-field');
       });
-      it('contains search field', function() {
-
+      it('Accepts input', function() {
+        const inputVal = 'barcelona';
+        cy.get('.input-field')
+        .type(inputVal)
+        .should('have.value',inputVal);
+      });
+      it('Filters activities', function() {
+        const inputVal = 'barcelona';
+        cy.get('.input-field')
+        .type(inputVal);
+        cy.get('.activities-grid').should('have.length',1);
+        cy.get('.input-field')
+        .type(inputVal);
+        cy.get('.activities-box').should('not.exist');
       });
     });
 

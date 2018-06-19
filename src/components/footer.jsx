577dd71c79dd6543d2a5bbaa18be62be789be0ee
george najm <georgenajm28@yahoo.com>
2018-06-18 23:11:35 -0400

    footer link updated to exact repo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,7 +9,7 @@ class Footer extends Component {
         <div className="footer-content">
           <div className="footer-text">by George Najm</div>
           <div className="footer-links">
-            <a href="http://github.com/georgesco94">
+            <a href="https://github.com/georgesco94/mixs">
               <i className="fab fa-github"></i></a>
             <a href="http://linkedin.com/in/george-najm-707003149/">
               <i className="fab fa-linkedin-in"></i></a>
